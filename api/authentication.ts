46931a45f85c946b546aa40fdfc626f067311b1c
noah <qny123@naver.com>
2023-08-20 16:54:09 +0900

    feat: member api 추가

--- api/authentication.ts
+++ api/authentication.ts
@@ -9,6 +9,14 @@ const authApis = {
     const response = await client.post(`${PATH}user/login`, data);
     return response;
   }),
+  signup: methodFormat(async (data) => {
+    const response = await client.post(`${PATH}user/signup`, data);
+    return response;
+  }),
+  enrollAccount: methodFormat(async (data) => {
+    const response = await client.post(`${PATH}account/enrollAccount`, data);
+    return response;
+  }),
 };
 
 export default authApis;
